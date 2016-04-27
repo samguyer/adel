ab51eb7d696fc48e3312e2ee11fa9f8d795c3fc7
Sam Guyer <sam.guyer@gmail.com>
2016-04-27 17:41:03 -0400

    Made a big mess, then cleaned it up
    Bottom line: wait needs to be 32-bit

--- adel.h
+++ adel.h
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <Arduino.h>
 
 #ifndef ADEL_V3
 #define ADEL_V3
@@ -16,7 +17,7 @@
 struct AdelAR
 {
   uint16_t line;
-  uint16_t wait;
+  uint32_t wait;
 };
 
 #define ADEL_FINALLY 0xFFFF
@@ -34,6 +35,15 @@ public:
   // -- Current function (index into stack)
   int current;
 
+  // -- Constructor: set everything to null
+  AdelRuntime() : current(0)
+  {
+    for (int i = 0; i < (1 << stack_depth); i++) {
+      stack[i] = 0;
+      ars[i] = 0;
+    }
+  }
+    
   // -- Helper method to initialize an activation record
   //    for the first time (note: uses malloc)
   AdelAR * init_ar(int index, int size_in_bytes)
@@ -43,7 +53,8 @@ public:
       ar = (AdelAR *) malloc(size_in_bytes);
       ars[index] = ar;
     }
-    memset(ar, 0, size_in_bytes);
+    ar->line = 0;
+    ar->wait = 0;
     stack[index] = ar;
     return ar;
   }
@@ -96,6 +107,19 @@ public:
   myRuntime.current = achild(c);		\
   res = f;
 
+#ifdef ADEL_DEBUG
+#define adel_debug(m, index, func, line)	\
+  Serial.print(m);				\
+  Serial.print(" in ");				\
+  Serial.print(func);				\
+  Serial.print("[");				\
+  Serial.print(index);				\
+  Serial.print("]:");				\
+  Serial.println(line)
+#else
+#define adel_debug(m, index, func, line)  ;
+#endif
+
 // ------------------------------------------------------------
 //   End-user macros
 
@@ -140,8 +164,10 @@ public:
   };									\
   int a_my_index = myRuntime.current;					\
   LocalAdelAR * a_me = (LocalAdelAR *) myRuntime.stack[a_my_index];	\
-  if (a_me == 0)							\
-    a_me = (LocalAdelAR *) myRuntime.init_ar(a_my_index, sizeof(LocalAdelAR));	\
+  if (a_me == 0) {							\
+    adel_debug("abegin", a_my_index, __FUNCTION__, __LINE__);		\
+    a_me = (LocalAdelAR *) myRuntime.init_ar(a_my_index,sizeof(LocalAdelAR));	\
+  }									\
   adel f_status, g_status;						\
   switch (my(line)) {							\
   case 0
@@ -150,33 +176,22 @@ public:
  *
  * Must be the last thing in the Adel function.
  */
-#define aend						\
-  default: ;						\
-  }							\
-  my(line) = ADEL_FINALLY;				\
+#define aend								\
+  case ADEL_FINALLY: ;							\
+  }									\
+  my(line) = 0;								\
+  adel_debug("aend", a_my_index, __FUNCTION__, __LINE__);		\
+  myRuntime.stack[a_my_index] = 0;					\
   return adel::DONE;
-
-/** afinally
- *
- *  Optionally, end with afinally to do some action whenever the function
- *  returns (for any reason)
-#define afinally( f )					\
-    a_me.line = __LINE__;				\
-    ainit_child(1);					\
-  case ADEL_FINALLY:					\
-    myRuntime.current = achild(1);				\
-    f_status = f;					\
-    if ( f_status.cont() ) return adel::CONT;
-*/
 
 /** adelay
  *
  *  Semantics: delay this function for t milliseconds
  */
-#define adelay(t)					\
-    my(line) = __LINE__;				\
-    my(wait) = millis() + t;				\
-  case __LINE__:					\
+#define adelay(t)							\
+    my(line) = __LINE__;						\
+    my(wait) = millis() + t;						\
+ case __LINE__:								\
     if (millis() < my(wait)) return adel::CONT;
 
 /** andthen
@@ -186,20 +201,22 @@ public:
  *     andthen( turn_on_light() );
  *     andthen( turn_off_light() );
  */
-#define andthen( f )					\
-    my(line) = __LINE__;				\
-    ainit(achild(1));					\
-  case __LINE__:					\
-    acall(f_status, 1, f);				\
+#define andthen( f )						\
+    my(line) = __LINE__;					\
+    ainit(achild(1));						\
+    adel_debug("andthen", a_my_index, __FUNCTION__, __LINE__);	\
+  case __LINE__:						\
+    acall(f_status, 1, f);					\
     if ( f_status.cont() ) return adel::CONT;
 
 /** awaituntil
  *  Wait asynchronously for a condition to become true. Note that this
  *  condition CANNOT be an adel function.
  */
-#define awaituntil( c )					\
-    my(line) = __LINE__;				\
-  case __LINE__:					\
+#define awaituntil( c )							\
+    my(line) = __LINE__;						\
+    adel_debug("awaituntil", a_my_index, __FUNCTION__, __LINE__);	\
+  case __LINE__:							\
     if ( ! ( c ) ) return adel::CONT
 
 /** aforatmost
@@ -217,6 +234,7 @@ public:
     my(line) = __LINE__;				\
     ainit(achild(1));					\
     my(wait) = millis() + t;				\
+    adel_debug("aforatmost", a_my_index, __FUNCTION__, __LINE__);	\
   case __LINE__:					\
     acall(f_status, 1, f);				\
     if (f_status.cont() && millis() < my(wait))		\
@@ -229,27 +247,29 @@ public:
  *  (both return false). Example use:
  *      atogether( flash_led(), play_sound() );
  */
-#define aboth( f , g )					\
-    my(line) = __LINE__;				\
-    ainit(achild(1));					\
-    ainit(achild(2));					\
-  case __LINE__: 					\
-    acall(f_status, 1, f);				\
-    acall(g_status, 2, g);				\
-    if (f_status.cont() || g_status.cont())		\
+#define aboth( f , g )						\
+    my(line) = __LINE__;					\
+    ainit(achild(1));						\
+    ainit(achild(2));						\
+    adel_debug("aboth", a_my_index, __FUNCTION__, __LINE__);	\
+  case __LINE__:						\
+   acall(f_status, 1, f);					\
+   acall(g_status, 2, g);					\
+   if (f_status.cont() || g_status.cont())			\
       return adel::CONT;
 
 /** adountil
  *
  *  Semantics: execute f until g completes.
  */
-#define adountil( f , g )				\
-    my(line) = __LINE__;				\
-    ainit(achild(1));					\
-    ainit(achild(2));					\
-  case __LINE__: 					\
-    acall(f_status, 1, f);				\
-    acall(g_status, 2, g);				\
+#define adountil( f , g )						\
+    my(line) = __LINE__;						\
+    ainit(achild(1));							\
+    ainit(achild(2));							\
+    adel_debug("adountil", a_my_index, __FUNCTION__, __LINE__);		\
+  case __LINE__:							\
+    acall(f_status, 1, f);						\
+    acall(g_status, 2, g);						\
     if (g_status.cont()) return adel::CONT;
 
 /** auntileither
@@ -266,15 +286,16 @@ public:
  *        // light finished first
  *     }
  */
-#define auntileither( f , g )				\
-    my(line) = __LINE__;				\
-    ainit(achild(1));					\
-    ainit(achild(2));					\
-  case __LINE__: 					\
-    acall(f_status, 1, f);				\
-    acall(g_status, 2, g);				\
-    if (f_status.cont() && g_status.cont())		\
-      return adel::CONT;				\
+#define auntileither( f , g )						\
+    my(line) = __LINE__;						\
+    ainit(achild(1));							\
+    ainit(achild(2));							\
+    adel_debug("auntileither", a_my_index, __FUNCTION__, __LINE__);	\
+  case __LINE__:							\
+    acall(f_status, 1, f);						\
+    acall(g_status, 2, g);						\
+    if (f_status.cont() && g_status.cont())				\
+      return adel::CONT;						\
     if (f_status.done())
 
 /** aforevery
@@ -286,41 +307,38 @@ public:
  *   aforevery( button(pin2), brighten(pin3) );
  *
  */
-#define aforevery( f , g )				\
-    my(line) = __LINE__;				\
-    ainit(achild(1));					\
-    ainit(achild(2));					\
-  case __LINE__: 					\
-    acall(f_status, 1, f);				\
-    if (f_status.cont()) return adel::CONT;		\
-    if (f_status.yield()) {				\
-      acall(g_status, 2, g);				\
-      my(line) = __LINE__;				\
-      return adel::CONT;				\
+#define aforevery( f , g )						\
+    my(line) = __LINE__;						\
+    ainit(achild(1));							\
+    ainit(achild(2));							\
+    adel_debug("aforevery", a_my_index, __FUNCTION__, __LINE__);	\
+  case __LINE__:							\
+    acall(f_status, 1, f);						\
+    if (f_status.cont()) return adel::CONT;				\
+    if (f_status.yield()) {						\
+      acall(g_status, 2, g);						\
+      my(line) = __LINE__;						\
+      return adel::CONT;						\
     }
 
 /** ayield
  *
  * Yield to the calling function. Use in combination with 
  */
-#define ayield				\
-    my(line) = __LINE__;		\
-    return adel::YIELD;	 		\
-  case __LINE__:
-
-/** acontinue
- *
- * Use inside a forevery block to continue calling the producer function.
- */
-#define acontinue return adel::CONT
+#define ayield							\
+    my(line) = __LINE__;					\
+    adel_debug("ayield", a_my_index, __FUNCTION__, __LINE__);	\
+    return adel::YIELD;						\
+  case __LINE__: ;
 
 /** afinish
  * 
  *  Semantics: leave the function immediately, and communicate to the
  *  caller that it is done.
  */
-#define afinish				   \
-    my(line) = ADEL_FINALLY;		   \
+#define afinish							\
+    my(line) = ADEL_FINALLY;					\
+    adel_debug("afinish", a_my_index, __FUNCTION__, __LINE__);	\
     return adel::CONT;
 
 #endif
